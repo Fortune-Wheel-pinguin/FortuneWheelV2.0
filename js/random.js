a9aaa2f06f14a896f026c68e600c4acdba3bb7a1
Jura <87808903+eljura111@users.noreply.github.com>
2021-10-29 09:48:26 +0200

    Add files via upload

---
+++ js/random.js
@@ -0,0 +1,25 @@
+let causas = [
+  "tu amigo y vecino spiderman muere a causa del guantele de thanos.",
+  "Atropellado por el autobus a 100km/h",
+];
+
+let victimas = ["pera", "manzana", "limon"];
+
+function randomNum(maximo) {
+  let num = Math.random() * maximo;
+  let redondearNum = Math.floor(num);
+
+  return redondearNum;
+}
+
+function imprimirVictima(identificador, lista) {
+  let localizacion = document.querySelector(identificador);
+  let indiceRandom = randomNum(lista.length);
+  let nombreRandom = lista[indiceRandom];
+
+  let html = nombreRandom;
+
+  localizacion.innerHTML = html;
+}
+imprimirVictima("#victima", victimas);
+imprimirVictima("#causaMortal", causas);
